bda509e9a99613f2de931dea2a7681e307a77daf
marcythebreadcat <bka9056@gmail.com>
2025-01-01 22:39:34 +0800

    fixed workflow

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/portfolio",
-  output: "export",  // <=== enables static exports
-
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // Required for static export
+  },
+  distDir: 'out',
 };
 
 export default nextConfig;
